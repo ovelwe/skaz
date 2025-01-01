27efd3aa8a2aa000858bc84c5263eb2fa5eaec16
ovelwe <alekpoli139@gmail.com>
2025-01-01 23:34:37 +0300

    added logo for README.md

--- src/main/java/net/ovelwe/skaz/items/ModCreativeModeTabs.java
+++ src/main/java/net/ovelwe/skaz/items/ModCreativeModeTabs.java
@@ -1,4 +1,28 @@
 package net.ovelwe.skaz.items;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.ovelwe.skaz.Skaz;
+
 public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS
+            = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Skaz.MODID);
+
+    public static final RegistryObject<CreativeModeTab> CREATIVE_MODE_TAB = CREATIVE_MODE_TABS.register("skaz_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.KSENTARIT.get()))
+                    .title(Component.translatable("creativetab.skaz_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.KSENTARIT.get());
+                        pOutput.accept(ModItems.GOLD_COIN.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
 }
--- src/main/java/net/ovelwe/skaz/items/ModItems.java
+++ src/main/java/net/ovelwe/skaz/items/ModItems.java
@@ -1,4 +1,23 @@
 package net.ovelwe.skaz.items;
 
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.ovelwe.skaz.Skaz;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Skaz.MODID);
+
+    public static final RegistryObject<Item> GOLD_COIN = ITEMS.register("gold_coin",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> KSENTARIT = ITEMS.register("ksentarit",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
